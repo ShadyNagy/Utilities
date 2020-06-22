3300bfae50f73a3f590688b0e22ca9c80b56c38a
Shady Nagy <shadynagi@gmail.com>
2020-06-22 22:21:06 +0200

    added Get Key Names in DbContextExtensions

--- src/ShadyNagy.Utilities.Extensions/DbContextExtensions.cs
+++ src/ShadyNagy.Utilities.Extensions/DbContextExtensions.cs
@@ -6,7 +6,7 @@ namespace ShadyNagy.Utilities.Extensions
     {
 
 #if NETFRAMEWORK
-        public static void Reset(this  System.Data.Entity.DbContext context)
+        public static void Reset(this System.Data.Entity.DbContext context)
         {
             var entries = context.ChangeTracker
                 .Entries()
@@ -29,6 +29,28 @@ namespace ShadyNagy.Utilities.Extensions
                 }
             }
         }
+
+        public static string[] GetKeyNames<TEntity>(this System.Data.Entity.DbContext context)
+            where TEntity : class
+        {
+            return context.GetKeyNames(typeof(TEntity));
+        }
+ 
+        public static string[] GetKeyNames(this System.Data.Entity.DbContext context, System.Type entityType)
+        {
+            var metadata = ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
+ 
+            // Get the mapping between CLR types and metadata OSpace
+            var objectItemCollection = ((ObjectItemCollection)metadata.GetItemCollection(DataSpace.OSpace));
+ 
+            // Get metadata for given CLR type
+            var entityMetadata = metadata
+                    .GetItems<EntityType>(DataSpace.OSpace)
+                    .Single(e => objectItemCollection.GetClrType(e) == entityType);
+ 
+            return entityMetadata.KeyProperties.Select(p => p.Name).ToArray();
+        }
+
 #else
         public static void Reset(this Microsoft.EntityFrameworkCore.DbContext context)
         {
