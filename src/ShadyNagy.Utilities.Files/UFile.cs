3766e60aac70c78040e40ea90651a59ce84d7042
Shady Nagy <shadynagi@gmail.com>
2020-06-22 21:15:17 +0200

    Utilities.Files added

---
+++ src/ShadyNagy.Utilities.Files/UFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ShadyNagy.Utilities.Files
+{
+    public class UFile
+    {
+        public static byte[] ReadBytes(string path, string fileName)
+        {
+            var fullPath = Path.Combine(path, fileName);
+
+            using (MemoryStream ms = new MemoryStream())
+            using (FileStream file = new FileStream(fullPath, FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+                ms.Write(bytes, 0, (int)file.Length);
+
+                return ms.ToArray();
+            }
+        }
+
+        public static void WriteBytes(string path, string fileName, byte[] data)
+        {
+            var fullPath = Path.Combine(path, fileName);
+
+            using (var file = new FileStream(fullPath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                file.Write(data, 0, (int)data.Length);
+
+            }
+        }
+    }
+}
