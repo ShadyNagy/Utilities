a163dedaba6ef3a146f0b7641de203ca59aab4ff
Shady Nagy <shadynagi@gmail.com>
2020-06-23 00:31:02 +0200

    Fixed GetKeyNames

--- src/ShadyNagy.Utilities.Extensions/DbContextExtensions.cs
+++ src/ShadyNagy.Utilities.Extensions/DbContextExtensions.cs
@@ -51,6 +51,36 @@ namespace ShadyNagy.Utilities.Extensions
             return entityMetadata.KeyProperties.Select(p => p.Name).ToArray();
         }
 
+        public static System.Type[] GetKeyTypes(this System.Data.Entity.DbContext context, System.Type entityType)
+        {
+            var metadata = ((System.Data.Entity.Infrastructure.IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
+ 
+            // Get the mapping between CLR types and metadata OSpace
+            var objectItemCollection = ((System.Data.Entity.Core.Metadata.Edm.ObjectItemCollection)metadata.GetItemCollection(System.Data.Entity.Core.Metadata.Edm.DataSpace.OSpace));
+ 
+            // Get metadata for given CLR type
+            var entityMetadata = metadata
+                    .GetItems<System.Data.Entity.Core.Metadata.Edm.EntityType>(System.Data.Entity.Core.Metadata.Edm.DataSpace.OSpace)
+                    .Single(e => objectItemCollection.GetClrType(e) == entityType);
+ 
+            var clrTypeMetadataPropName = @"http://schemas.microsoft.com/ado/2013/11/edm/customannotation:ClrType";
+            return entityMetadata.KeyProperties.Select(p => (System.Type) p.MetadataProperties.Single(z => z.Name == clrTypeMetadataPropName).Value).ToArray();
+        }
+
+        public static System.Collections.Generic.IEnumerable<object> GetEntityKeysValue<TEntity>(this System.Data.Entity.DbContext context, TEntity entity)
+          where TEntity : class
+        {
+          if (context == null)
+            throw new System.NullReferenceException("context");
+
+          var type = typeof(TEntity);
+
+          var set = ((System.Data.Entity.Infrastructure.IObjectContextAdapter)context).ObjectContext.CreateObjectSet<TEntity>();
+          var entitySet = set.EntitySet;
+          var keys = entitySet.ElementType.KeyMembers;
+          var props = keys.Select(k => type.GetProperty(k.Name));
+          return props.Select(p => p.GetValue(entity));
+        }
 #else
         public static void Reset(this Microsoft.EntityFrameworkCore.DbContext context)
         {
