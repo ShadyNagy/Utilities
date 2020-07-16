c0f5f2695ee6051d043a24e150a17b49601f9c82
Shady Nagy <shadynagi@gmail.com>
2020-07-16 02:24:19 +0200

    added DataTableCollectionExtensions

---
+++ src/ShadyNagy.Utilities.Extensions/DataTableCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ShadyNagy.Utilities.Extensions
+{
+    public static class DataTableCollectionExtensions
+    {
+
+        public static DataTable GetTable(this DataTableCollection tables, string tableName) =>
+            tables == null ? new DataTable() :
+                tables
+                .Cast<DataTable>()
+                .FirstOrDefault(x => x.TableName.ToLower() == tableName.ToLower());
+
+        public static DataTable GetTable(this DataTableCollection tables, int tableNumber) =>
+            tables == null || tables.Count <= tableNumber ? new DataTable() : tables[tableNumber];
+
+        public static IEnumerable<DataColumn> GetColumns(this DataTableCollection tables, string tableName) =>
+            tables == null ? new List<DataColumn>() :
+                tables
+                .GetTable(tableName)
+                .Columns
+                .Cast<DataColumn>()
+                .ToList();
+
+        public static IEnumerable<DataColumn> GetColumns(this DataTableCollection tables, int tableNumber) =>
+            tables == null ? new List<DataColumn>() :
+                tables
+                    .GetTable(tableNumber)
+                    .Columns
+                    .Cast<DataColumn>()
+                    .ToList();
+
+        public static IEnumerable<string> GetColumnsNames(this DataTableCollection tables, string tableName) =>
+            tables
+                .GetColumns(tableName)
+                .Select(x => x.Caption)
+                .ToList();
+
+        public static IEnumerable<string> GetColumnsNames(this DataTableCollection tables, int tableNumber) =>
+            tables
+                .GetColumns(tableNumber)
+                .Select(x => x.Caption)
+                .ToList();
+    }
+}
