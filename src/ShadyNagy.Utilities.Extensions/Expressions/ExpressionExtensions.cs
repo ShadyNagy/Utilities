0bb53efa9ffc3ab487446715e16bfac4d5b178c3
ShadyNagy <info@shadynagy.com>
2020-11-26 13:55:21 +0200

    Expression Extensions added.

---
+++ src/ShadyNagy.Utilities.Extensions/Expressions/ExpressionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace ShadyNagy.Utilities.Extensions.Expressions
+{
+    public static class ExpressionExtensions
+    {
+        public static MemberExpression GetNestedProperty(this Expression param, string property)
+        {
+            var propNames = property.Split('.');
+            var propExpr = Expression.Property(param, propNames[0]);
+
+            for (var i = 1; i < propNames.Length; i++)
+            {
+                propExpr = Expression.Property(propExpr, propNames[i]);
+            }
+
+            return propExpr;
+        }
+
+    }
+}
