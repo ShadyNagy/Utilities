d0c90ce118fb7b7fe1999e3f02222fe5dcb04537
ShadyNagy <info@shadynagy.com>
2020-11-26 14:01:41 +0200

    Added Dynamic Specification

---
+++ src/ShadyNagy.Utilities.DesignPatterns/Specification/FilterOperator.cs
@@ -0,0 +1,15 @@
+﻿namespace ShadyNagy.Utilities.DesignPatterns.Specification
+{
+    public enum FilterOperator
+    {
+        Equals,
+        NotEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LessThan,
+        LessThanOrEqual,
+        Contains,
+        StartsWith,
+        EndsWith
+    }
+}
--- src/ShadyNagy.Utilities.DesignPatterns/Specification/Specification.cs
+++ src/ShadyNagy.Utilities.DesignPatterns/Specification/Specification.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection;
+using ShadyNagy.Utilities.Extensions.Expressions;
 
 namespace ShadyNagy.Utilities.DesignPatterns.Specification
 {
 
     public abstract class Specification<T, T2>
     {
+        private static readonly Type _stringType = typeof(string);
+
+        private static readonly MethodInfo _toStringMethod = typeof(object).GetMethod("ToString");
+
+        private static readonly MethodInfo _containsMethod = typeof(string).GetMethod("Contains"
+            , new Type[] { typeof(string) });
+
+        private static readonly MethodInfo _endsWithMethod
+            = typeof(string).GetMethod("EndsWith", new Type[] { typeof(string) });
+
+        private static readonly MethodInfo _startsWithMethod
+            = typeof(string).GetMethod("StartsWith", new Type[] { typeof(string) });
+
 
         public static readonly Specification<T, T2> All = new IdentitySpecification<T, T2>();
-
+        public string PropertyName { get; set; }
+        public FilterOperator FilterOperator { get; set; }
+        public object Value { get; set; }
 
         public T2 IsSatisfiedBy(T entity)
         {
@@ -17,7 +34,33 @@ namespace ShadyNagy.Utilities.DesignPatterns.Specification
         }
 
 
-        public abstract Expression<Func<T, T2>> ToExpression();
+        public virtual Expression<Func<T, T2>> ToExpression()
+        {
+            if (typeof(T2) == typeof(bool))
+            {
+                if (string.IsNullOrEmpty(PropertyName))
+                {
+                    return x => (T2)(object)Convert.ToBoolean(true);
+                }
+                else
+                {
+                    var param = Expression.Parameter(typeof(T));
+                    return Expression.Lambda<Func<T, T2>>(GetFilter(param, PropertyName, FilterOperator, Value), param);
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(PropertyName))
+                {
+                    return x => default;
+                }
+                else
+                {
+                    var param = Expression.Parameter(typeof(T));
+                    return Expression.Lambda<Func<T, T2>>(GetPropertyGetter<T>(PropertyName), param);
+                }
+            }
+        }
 
         public Specification<T, T2> And(Specification<T, T2> specification)
         {
@@ -40,6 +83,61 @@ namespace ShadyNagy.Utilities.DesignPatterns.Specification
         public Specification<T, T2> Not()
         {
             return new NotSpecification<T, T2>(this);
+        }
+
+        public static Expression<Func<TEntity, object>> GetPropertyGetter<TEntity>(string property)
+        {
+            if (property == null)
+                throw new ArgumentNullException(nameof(property));
+
+            var param = Expression.Parameter(typeof(TEntity));
+            var prop = param.GetNestedProperty(property);
+            var convertedProp = Expression.Convert(prop, typeof(object));
+            return Expression.Lambda<Func<TEntity, object>>(convertedProp, param);
+        }
+
+        internal static Expression GetFilter(ParameterExpression param, string property, FilterOperator op, object value)
+        {
+            var constant = Expression.Constant(value);
+            var prop = param.GetNestedProperty(property);
+            return CreateFilter(prop, op, constant);
+        }
+
+        private static Expression CreateFilter(MemberExpression prop, FilterOperator filterOperator, ConstantExpression constant)
+        {
+            switch (filterOperator)
+            {
+                case FilterOperator.Equals:
+                    return Expression.Equal(prop, constant);
+                case FilterOperator.GreaterThan:
+                    return Expression.GreaterThan(prop, constant);
+                case FilterOperator.LessThan:
+                    return Expression.LessThan(prop, constant);
+                case FilterOperator.Contains:
+                    return Expression.Call(prop, _containsMethod, PrepareConstant(constant));
+                case FilterOperator.StartsWith:
+                    return Expression.Call(prop, _startsWithMethod, PrepareConstant(constant));
+                case FilterOperator.EndsWith:
+                    return Expression.Call(prop, _endsWithMethod, PrepareConstant(constant));
+                case FilterOperator.NotEqual:
+                    return Expression.NotEqual(prop, constant);
+                case FilterOperator.GreaterThanOrEqual:
+                    return Expression.GreaterThanOrEqual(prop, constant);
+                case FilterOperator.LessThanOrEqual:
+                    return Expression.LessThanOrEqual(prop, constant);
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static Expression PrepareConstant(ConstantExpression constant)
+        {
+            if (constant.Type == _stringType)
+                return constant;
+
+            var convertedExpr = Expression.Convert(constant, typeof(object));
+            return Expression.Call(convertedExpr, _toStringMethod);
         }
     }
 }
