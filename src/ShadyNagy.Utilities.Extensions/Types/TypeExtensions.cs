b95bd869ae04c6fdddc8360bd3e90af59f60f285
ShadyNagy <info@shadynagy.com>
2020-12-04 15:59:24 +0200

    TypeExtensions added.

---
+++ src/ShadyNagy.Utilities.Extensions/Types/TypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using ShadyNagy.Utilities.Extensions.Object;
+
+namespace ShadyNagy.Utilities.Extensions.Types
+{
+    public static class TypeExtensions
+    {
+        public static PropertyInfo GetRuntimePropertyWithoutCase(this Type type, string property)
+        {
+            var propertyWithoutCase = type.GetPropertyName(property);
+            if (propertyWithoutCase == null)
+            {
+                return null;
+            }
+
+            return type.GetRuntimeProperty(propertyWithoutCase);
+        }
+    }
+}
